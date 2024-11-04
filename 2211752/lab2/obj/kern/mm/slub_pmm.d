3716c13b0c2970cb9705a7a543b6049459d42a3a
weiyu12123 <364716692@qq.com>
2024-11-04 21:01:10 +0800

    1

--- 2211752/lab2/obj/kern/mm/slub_pmm.d
+++
@@ -1,4 +0,0 @@
-obj/kern/mm/slub_pmm.o obj/kern/mm/slub_pmm.d: kern/mm/slub_pmm.c \
- libs/stdio.h libs/defs.h libs/stdarg.h kern/mm/pmm.h kern/debug/assert.h \
- libs/atomic.h kern/mm/memlayout.h libs/list.h kern/mm/mmu.h libs/riscv.h \
- libs/string.h kern/mm/slub_pmm.h
